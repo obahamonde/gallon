a4abd3c534c69868c67bb02d20503395701a9616
Ubuntu <ubuntu@ip-172-31-25-120.ec2.internal>
2023-09-07 23:47:44 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+from gallon import Gallon, GallonClient, Request
+
+URL = "https://api.openai.com/v1/chat/completions"
+
+client = GallonClient()
+
+app = Gallon()
+
+@app.get("/")
+def ask(request:Request):
+	print(request.query)
+	text = request.path_params
+	response = client.post(
+		URL,
+		headers={"Authorization": "Bearer " + os.environ["OPENAI_API_KEY"], "Content-Type": "application/json"},
+		body={
+			"messages":[
+				{
+					"role":"user",
+					"content":text,
+				}
+			],
+			"model":"gpt-4-0613",
+			"max_tokens":2048,
+		},
+	)
+	return response
+
+app.run()
